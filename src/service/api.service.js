3f09a62d7eadb1c59a4980675b5f016e599ec127
ArinaBogan <arina.bogan@gmail.com>
2023-11-23 22:06:05 +0300

    add task

---
+++ src/service/api.service.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcrypt');
+const { getUserByEmail,createUserDB } = require('../repository/api.repository')
+const salt = 10;
+
+async function createUser(name, surname, email, pwd) {
+    const user = await getUserByEmail(email)
+    if (user.length) throw new Error('user has already exist');
+
+    const hashPWD = await bcrypt.hash(pwd, salt);
+
+    const data = await createUserDB(name, surname, email, hashPWD)
+    if (!data.length) throw new Error('not created')
+    return data
+
+}
+
+module.exports = { createUser }
